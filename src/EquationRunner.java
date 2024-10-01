b1cc8ae0a2d8e8c9d722658a5b802baea382bfad
unknown <BT_2S14_19@BKTECH.LOCAL>
2024-10-01 11:05:16 -0400

    Initial commit

---
+++ src/EquationRunner.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class EquationRunner {
+    public static void main(String[] args) {
+        String coordinates;
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter your first Coordinate Point: ");
+        String coordinates = s.nextLine();
+
+    }
+}
