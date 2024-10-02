4039d8dc5e9a571230b25372a42e0d19cd42b259
BT_2S14_19 <davidwg1210@gmail.com>
2024-10-02 11:05:57 -0400

    added stuff

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -2,11 +2,22 @@ import java.util.Scanner;
 
 public class EquationRunner {
     public static void main(String[] args) {
-        String coordinates;
         Scanner s = new Scanner(System.in);
 
         System.out.println("Enter your first Coordinate Point: ");
-        String coordinates = s.nextLine();
+        String firstCoordinate = s.nextLine();
 
+
+        LinearEquation coordinate1 = new LinearEquation();
+        coordinate1.x1y1(firstCoordinate);
+
+        System.out.println("Enter your second Coordinate Point: ");
+        String secondCoordinate = s.nextLine();
+        LinearEquation coordinate2 = new LinearEquation();
+        coordinate2.x2y2(secondCoordinate);
+
+        System.out.println(coordinate1);
+        System.out.println(coordinate2.toString2());
     }
+
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,47 @@
 public class LinearEquation {
+
+
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+    private double m;
+    private double yInt;
+    private String slope;
+    private int index;
+
+    public void x1y1 (String input){
+    int length = input.length()-1;
+    String a = input.substring(1, length);
+    int index = input.indexOf(",") -1;
+    this.x1 = Integer.parseInt(a.substring(0, index));
+    String c = a.substring(input.indexOf(","));
+    this.y1 = Integer.parseInt(c);
+    }
+
+    public void x2y2 (String input){
+        int length = input.length()-1;
+        String b = input.substring(1, length);
+        int index = input.indexOf(",") -1;
+        this.x2 = Integer.parseInt(b.substring(0, index));
+        String d = b.substring(input.indexOf(","));
+        this.y2 = Integer.parseInt(d);
+    }
+
+    public int x1(){
+        return x1;
+    }
+
+    public LinearEquation(int x, int y){
+
+    }
+
+
+    public String toString(){
+       String str = this.x1+","+this.y1;
+       return str;
+    }
+    public String toString2(){
+        return this.x2+","+this.y2;
+    }
 }
