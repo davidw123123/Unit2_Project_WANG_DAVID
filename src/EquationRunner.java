01e734717d385948fbc17a7ce207ba0b5bf94371
davidw123123 <davidwg1210@gmail.com>
2024-10-05 21:54:54 -0400

    added more methods

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -2,22 +2,25 @@ import java.util.Scanner;
 
 public class EquationRunner {
     public static void main(String[] args) {
+
+
+
         Scanner s = new Scanner(System.in);
+        LinearEquation coordinate = new LinearEquation();
 
-        System.out.println("Enter your first Coordinate Point: ");
+        // asks for first coordinate point, then substrings
+        System.out.print("Enter your first Coordinate Point: ");
         String firstCoordinate = s.nextLine();
+        coordinate.x1y1(firstCoordinate);
 
-
-        LinearEquation coordinate1 = new LinearEquation();
-        coordinate1.x1y1(firstCoordinate);
-
-        System.out.println("Enter your second Coordinate Point: ");
+        // asks for second coordinate point, then substrings
+        System.out.print("Enter your second Coordinate Point: ");
         String secondCoordinate = s.nextLine();
-        LinearEquation coordinate2 = new LinearEquation();
-        coordinate2.x2y2(secondCoordinate);
+        coordinate.x2y2(secondCoordinate);
 
-        System.out.println(coordinate1);
-        System.out.println(coordinate2.toString2());
+        LinearEquation result = new LinearEquation(coordinate.x1(), coordinate.y1(),coordinate.x2(), coordinate.y2());
+
+        System.out.println(result.toString());
     }
 
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -5,10 +5,29 @@ public class LinearEquation {
     private int y1;
     private int x2;
     private int y2;
-    private double m;
+    private String m;
     private double yInt;
-    private String slope;
+    private double slope;
     private int index;
+    private double distance;
+    private double temp;
+    private double a;
+    private String b;
+    private String c;
+    private int b1;
+    private int b2;
+
+    public LinearEquation(int x1, int y1, int x2, int y2){
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        distanceAndSlope();
+        yIntercept();
+    }
+
+    public LinearEquation() {
+    }
 
     public void x1y1 (String input){
     int length = input.length()-1;
@@ -28,20 +47,80 @@ public class LinearEquation {
         this.y2 = Integer.parseInt(d);
     }
 
+    // to calculate the distance and slope
+    public void distanceAndSlope(){
+        distance = Math.sqrt(Math.pow((this.x1 - this.x2),2)+Math.pow((this.y1 -this.y2), 2));
+        b =String.valueOf(this.y1-this.y2);
+        b1 = (this.y1-this.y2);
+        c = String.valueOf(this.x1-this.x2);
+        b2 = (this.x1-this.x2);
+
+        this.m = b + "/"+c;
+
+        a = ((double) this.y1-this.y2)/((double) this.x1-this.x2);
+
+        if ()
+
+//        if (b1 <0  && b2 < 0 && b1 != b2) {
+//            b2 = b2*-1;
+//            b1 = b1 *-1;
+//            c = String.valueOf(b2);
+//            b = String.valueOf(b1);
+//            this.m = b + "/"+c;
+//        }else {
+//            if (b2 < 0 & b1 != b2) {
+//                b1 = b1 * -1;
+//                b2 = b2 * -1;
+//                c = String.valueOf(b2);
+//                b = String.valueOf(b1);
+//                this.m = b + "/" + c;
+//                if ((Math.abs(b1) / b2) % 1 == 0) {
+//                    this.m = String.valueOf(b1 / b2);
+//                } else {
+//                    if (b1 == b2) {
+//                        this.m = "1";
+//                    } else {
+//                if ( (Math.abs(b1)/b2) % 1 == 0 ){
+//                    this.m = "-"+String.valueOf(b1/b2);
+//                }
+//
+//                    }
+//                }
+//            }
+//        }
+
+
+    }
+
+    public void yIntercept(){
+        this.temp = ((double) this.x1* a);
+        if (temp < 0) {
+            yInt = this.y1 + Math.abs(this.temp);
+        }else {
+            yInt = this.y1 - temp;
+        }
+
+    }
+
     public int x1(){
         return x1;
     }
-
-    public LinearEquation(int x, int y){
-
+    public int y1(){
+        return y1;
     }
-
+    public int x2(){
+        return x2;
+    }
+    public int y2(){
+        return y2;
+    }
 
     public String toString(){
-       String str = this.x1+","+this.y1;
+       String str = "\n"+"First Pair: (" +this.x1+","+this.y1+")"+"\n"+
+                    "Second Pair: ("+this.x2+","+this.y2+ ")"+"\n"+
+                    "The distance between these 2 points is: "+ distance + "\n"+
+                    "The Slope is: "+this.m+"\n"+
+                    "The Y intercept is: " + yInt+"\n"+b+" "+ c;
        return str;
-    }
-    public String toString2(){
-        return this.x2+","+this.y2;
     }
 }
